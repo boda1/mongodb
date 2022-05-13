3a6df7042e73bda3df6b83dfe7f2dc4b701576e6
Adam Boda <adam.boda@MBP-ADAMBODA.local>
2022-05-13 11:26:16 +0100

    moving connection from .env to .js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
