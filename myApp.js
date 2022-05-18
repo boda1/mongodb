3d2fd16637a15069e0bc2a5bb7a4b1ca799415aa
Adam Boda <adam.boda@MBP-ADAMBODA.local>
2022-05-18 07:50:03 +0100

    fixed env file

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
-const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
+
+console.log("hello");
 
 let Person;
 
